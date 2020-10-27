7a0ea9fe3e09c14348f92957caee394b8fecebd6
Dominique <domi.github@gmail.com>
2020-10-27 23:36:53 +0100

    feat(native): mmove native to own implementation

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,89 @@
  */
 
 import * as React from 'react';
-import { CommonStackProps, spacerFactory, SpacerProps, stackFactory } from './stacks';
 
 /**
  * Spacer
  */
-export { SpacerProps } from './stacks';
-export const Spacer: React.FunctionComponent<SpacerProps> = spacerFactory('div', 'px');
+export type SpacerProps = { spacing?: number; variant: 'row' | 'column' };
+export const Spacer: React.FunctionComponent<SpacerProps> = props => (
+	<div
+		style={{
+			height: props.variant === 'column' ? props.spacing : '100%',
+			width: props.variant === 'column' ? '100%' : props.spacing,
+			flexShrink: 0,
+			flexGrow: 0,
+		}}
+	/>
+);
 
-export type StackProps = CommonStackProps & React.HTMLAttributes<HTMLDivElement>;
-export const Stack: React.FC<StackProps> = stackFactory('div', 'px');
+/**
+ * Stacks
+ */
+type FlexAlignType = 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+type FlexJustifyType =
+	| 'flex-start'
+	| 'flex-end'
+	| 'center'
+	| 'space-between'
+	| 'space-around'
+	| 'space-evenly';
+
+export type StackProps = {
+	spacing?: number;
+	padding?: number;
+	margin?: number;
+	flex?: boolean;
+	alignItems?: FlexAlignType;
+	justifyContent?: FlexJustifyType;
+	variant: 'row' | 'column';
+	debug?: boolean;
+} & React.HTMLProps<HTMLDivElement>;
+
+export const Stack: React.FC<StackProps> = props => {
+	const {
+		children,
+		spacing = 0,
+		padding,
+		margin,
+		style,
+		variant,
+		alignItems,
+		justifyContent,
+		flex = false,
+		debug = false,
+		...restProps
+	} = props as any;
+	const oldChildren = React.Children.toArray(children);
+	const newChildren: React.ReactNode[] = [];
+	for (let i = 0; i < oldChildren.length; i++) {
+		newChildren.push(oldChildren[i]);
+		if (i < oldChildren.length - 1 && spacing !== 0) {
+			newChildren.push(
+				<Spacer variant={variant} key={`${variant}_stack_${i}`} spacing={spacing} />
+			);
+		}
+	}
+	return (
+		<div
+			style={[
+				flex && { flex: 1 },
+				{
+					padding,
+					margin,
+					flexDirection: variant,
+					alignItems,
+					justifyContent,
+				},
+				style,
+				debug && { backgroundColor: 'green' },
+			]}
+			{...restProps}
+		>
+			{newChildren}
+		</div>
+	);
+};
 
 type StackVariantProps = Omit<StackProps, 'variant'>;
 export const VStack: React.FunctionComponent<StackVariantProps> = props => (
